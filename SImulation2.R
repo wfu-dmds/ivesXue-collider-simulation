dd41f50cbe120a9bdd1a4e8e531fbb749b27aa63
Lucy D'Agostino McGowan <mcgowald@wfu.edu>
2024-03-28 11:08:29 -0400

    update code to adapt model to only have pre-exposure protienuria

--- SImulation2.R
+++ SImulation2.R
@@ -3,6 +3,7 @@ library(visreg)     # To visualize regression output
 library(ggplot2)    # For general plotting
 library(gridExtra)  # To combine plots
 library(forestplot) # To make forest plots
+library(ggdag)
 
 # Step 2: Original Data Generation and Analysis
 # Box 1 and Box 2 from original script remain unchanged
@@ -15,6 +16,19 @@ A <- 0.5 * W + rnorm(N)
 Y <- 0.3 * A + 0.4 * W + rnorm(N)
 fit1 <- lm(Y ~ A)
 fit2 <- lm(Y ~ A + W)
+## Y: outcome
+## X: exposure
+## W: confounder
+dag_1 <- dagify(
+  A ~ W,
+  Y ~ A + W,
+  exposure = "A",
+  outcome = "Y",
+  coords = time_ordered_coords()
+)
+
+ggdag(dag_1)
+
 g1 <- visreg(fit1, "A", gg = TRUE, line = list(col = "blue"),
              points = list(size = 2, pch = 1, col = "black")) + 
   theme_classic() +
@@ -33,6 +47,17 @@ Y <- 0.3 * A + rnorm(N)
 C <- 1.2 * A + 0.9 * Y + rnorm(N)
 fit3 <- lm(Y ~ A)
 fit4 <- lm(Y ~ A + C)
+## A: exposure
+## Y: outcome
+## C: collider
+dag_2 <- dagify(
+  Y ~ A,
+  C ~ A + Y,
+  exposure = "A",
+  outcome = "Y",
+  coords = time_ordered_coords()
+)
+ggdag(dag_2)
 g4 <- visreg(fit4, "A", gg = TRUE, line = list(col = "red"),
              points = list(size = 2, pch = 1, col = "black")) + 
   theme_classic() +
@@ -44,6 +69,16 @@ plot(g4)
 grid.arrange(g2, g4, ncol = 2)
 
 # Box 3: Data generation for proteinuria example (Original)
+dag_3 <- dagify(
+  Sodium_gr ~ Age_years,
+  sbp_in_mmHg ~ Sodium_gr + Age_years,
+  Proteinureia_in_mg ~ sbp_in_mmHg + Sodium_gr,
+  exposure = "Sodium_gr",
+  outcome = "sbp_in_mmHg",
+  coords = time_ordered_coords()
+)
+ggdag(dag_3)
+
 generateData <- function(n, seed) {
   set.seed(seed)
   Age_years <- rnorm(n, 65, 5)
@@ -192,30 +227,90 @@ lci <- (mean(collider) - 1.96 * mean(se))
 uci <- (mean(collider) + 1.96 * mean(se))
 
 # Step 3: Adapted Data Generation with New Variables
-generateDataExtended <- function(n, seed) {
+## original version:
+## time 0: age
+## time 1: sodium
+## time 2: sbp
+## time 3: proteinuria
+
+generateData <- function(n, seed) {
   set.seed(seed)
   Age_years <- rnorm(n, 65, 5)
-  Sodium_time0 <- rnorm(n) # Simulated sodium intake at time 0
-  SBP_time1 <- 1.05 * Sodium_time0 + rnorm(n) # Simulated SBP at time 1
-  Proteinuria_time2 <- 2.00 * SBP_time1 + 2.80 * Sodium_time0 + rnorm(n) # Simulated proteinuria at time 2
   Sodium_gr <- Age_years / 18 + rnorm(n)
   sbp_in_mmHg <- 1.05 * Sodium_gr + 2.00 * Age_years + rnorm(n)
   hypertension <- ifelse(sbp_in_mmHg >= 140, 1, 0)
-  data.frame(sbp_in_mmHg, hypertension, Sodium_gr, Age_years, Proteinuria_time2, SBP_time1, Sodium_time0)
+  Proteinuria_in_mg <- 2.00 * sbp_in_mmHg + 2.80 * Sodium_gr + rnorm(n)
+  data.frame(sbp_in_mmHg, hypertension, Sodium_gr, Age_years, Proteinuria_in_mg)
+}
+
+new_dag <- dagify(
+  sodium_0 ~ age,
+  sbp_1 ~ age + sodium_0,
+  sodium_1 ~ age + sodium_0,
+  proteinuria_2 ~ sodium_1 + sbp_1,
+  sodium_2 ~ age + sodium_1,
+  sbp_2 ~ age + sbp_1 + sodium_1,
+  proteinuria_3 ~ sodium_2 + sbp_2,
+  sbp_3 ~ age + sbp_2 + sodium_2,
+  exposure = "sodium_1",
+  outcome = "sbp_2",
+  coords = time_ordered_coords()
+)
+
+ggdag(new_dag)
+ggdag_adjustment_set(adjust_for(new_dag, "age"))
+
+new_dag <- dagify(
+  sodium_neg1 ~ age,
+  sbp_0 ~ age + sodium_neg1,
+  proteinuria_1 ~ sodium_neg1 + sbp_0,
+  sodium_0 ~ sodium_neg1,
+  sbp_1 ~ age + sodium_0,
+  proteinuria_2 ~ sodium_0 + sbp_1,
+  exposure = "sodium_0",
+  outcome = "sbp_1",
+  coords = time_ordered_coords()
+)
+
+ggdag(new_dag)
+ggdag_adjustment_set(new_dag)
+generateDataExtended <- function(n, seed) {
+  set.seed(seed)
+  Age_years <- rnorm(n, 64)
+  Sodium_gr_neg1 <- Age_years / 18 + rnorm(n, sd = sqrt(0.5))
+  sbp_in_mmHg_0 <-  1.05 * Sodium_gr_neg1 + 2.00 * Age_years + rnorm(n)
+  Proteinuria_in_mg_1 <- 2.00 * sbp_in_mmHg_0 + 2.80 * Sodium_gr_neg1 + rnorm(n)
+  Sodium_gr_0 <- Sodium_gr_neg1 + rnorm(n, sd = sqrt(0.5))
+  sbp_in_mmHg_1 <- 1.05 * Sodium_gr_0 + 2.00 * Age_years + rnorm(n)
+  Proteinuria_in_mg_2 <- 2.00 * sbp_in_mmHg_1 + 2.80 * Sodium_gr_0 + rnorm(n)
+  hypertension <- ifelse(sbp_in_mmHg_1 >= 140, 1, 0)
+  data.frame(sbp_in_mmHg_1, hypertension, Sodium_gr_0, Age_years, Sodium_gr_neg1,
+             sbp_in_mmHg_0, Proteinuria_in_mg_1, Proteinuria_in_mg_2)
 }
 
 # Step 4: Adapted Regression Analyses
 ObsDataExtended <- generateDataExtended(n = 1000, seed = 777)
 
-fit6 <- lm(sbp_in_mmHg ~ Sodium_gr + Age_years + Proteinuria_time2, data = ObsDataExtended)
-
+fit6 <- lm(sbp_in_mmHg_1 ~ Sodium_gr_0 + Age_years + Proteinuria_in_mg_2, data = ObsDataExtended)
+fit6
+fit7 <- lm(sbp_in_mmHg_1 ~ Sodium_gr_0 + Age_years + Proteinuria_in_mg_1, data = ObsDataExtended)
+fit7
 # Step 5: Visualizations and Comparisons
 # Adapted Model Visualization
-m6 <- visreg(fit6, "Sodium_gr", gg = TRUE, xlab = "Sodium at Time 3 (gr)", ylab = "SBP at Time 4 (mmHg)",
+m6 <- visreg(fit6, "Sodium_gr_0", gg = TRUE, xlab = "Sodium at Time 0 (gr)", ylab = "SBP at Time 1 (mmHg)",
              line = list(col = "red"),
              points = list(size = 2, pch = 1, col = "black"), bty = "n") + 
   theme_classic() +
-  ggtitle("Adapted Model with Early Proteinuria")
+  ggtitle("Adapted Model with Post-Exposure/Outcome Proteinuria")
 
 # Visualization of the adapted model
 print(m6)
+
+m7 <- visreg(fit7, "Sodium_gr_0", gg = TRUE, xlab = "Sodium at Time 0 (gr)", ylab = "SBP at Time 1 (mmHg)",
+             line = list(col = "red"),
+             points = list(size = 2, pch = 1, col = "black"), bty = "n") + 
+  theme_classic() +
+  ggtitle("Adapted Model with Pre-Exposure/Outcome Proteinuria")
+
+# Visualization of the adapted model
+print(m7)
